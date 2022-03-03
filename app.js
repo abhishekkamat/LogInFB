8940f848dabf21365f93aa07c58127c617e8e4d3
abhishekkamat <96072575+abhishekkamat@users.noreply.github.com>
2022-03-03 16:02:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-function statusCheck(){
-    FB.getLoginStatus(function(response) {
-       console.log(response);
-        });
+function windowOpen(){
+    pass=getSessionID();
+    appID="489702302849005";
+    redirectURI="https://www.facebook.com/connect/login_success.html";
+    OGURL="https://www.facebook.com/v13.0/dialog/oauth?client_id={"+appID+"}&redirect_uri={"+redirectURI+"}&state={"+pass+"}";
+    window.open(OGURL, "Log Into Facebook","width=500, height=500, left=200, top=50");
+}
+
+function clickCounter(){
+    if(typeof(Storage)!=="undefined"){
+        if(sessionStorage.clickcount){
+            sessionStorage.clickcount=Number(sessionStorage.clickcount)+1;
+        }
+        else{
+            sessionStorage.clickcount=1;
+        }
+        document.getElementById("result").innerHTML="You have clicked "+sessionStorage.clickcount+" times in this session.";
+    }
+    else{
+        document.getElementById("result").innerHTML="You dont have session storage";
+    }
+}
+function Pass(){
+    sessionID=getSessionID();
+    document.getElementById("password").innerHTML="Generated Password= "+sessionID;
+}
+function getSessionID(){
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var passwordLength = 12;
+ var password = "";
+
+ for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+   }
+
+   return password;
 }
